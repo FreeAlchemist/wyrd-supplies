9e8d0032d61cdd910abe3faf9a4bb17db565b9c7
FreeAlchemist <freealchemist@mail.ru>
2016-04-28 10:39:30 +0300

    Create pursuits-into-the-bayou.js

---
+++ ttb-pursuits/js/pursuits-into-the-bayou.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Pursuits: Into the Bayou</title>
+	<meta charset="UTF-8"/>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+<div id="page"></div>
+<script type="text/javascript" src="js/pursuits-into-the-bayou.js"></script>
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/cardsCreate.js"></script>
+</body>
+</html>

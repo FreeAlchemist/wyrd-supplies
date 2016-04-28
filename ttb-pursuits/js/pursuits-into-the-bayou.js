216c7a5586d2114e0a757754f885053b506c59cd
FreeAlchemist <freealchemist@mail.ru>
2016-04-28 10:40:10 +0300

    Update pursuits-into-the-bayou.js

--- ttb-pursuits/js/pursuits-into-the-bayou.js
+++ ttb-pursuits/js/pursuits-into-the-bayou.js
@@ -1,14 +1,25 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Pursuits: Into the Bayou</title>
-	<meta charset="UTF-8"/>
-	<link rel="stylesheet" type="text/css" href="css/main.css">
-</head>
-<body>
-<div id="page"></div>
-<script type="text/javascript" src="js/pursuits-into-the-bayou.js"></script>
-<script type="text/javascript" src="js/jquery.min.js"></script>
-<script type="text/javascript" src="js/cardsCreate.js"></script>
-</body>
-</html>
+var pursuit = {
+	type:'Into The Bayou',
+	faction:'gremlin',
+	'bokor' : {
+		name: 'bokor',
+		text:'bokor text',
+		gear:'',
+		talent:{
+			name:'educated',
+			text:'Whenever this character...bokor'
+		},
+		step:{
+			'1' : 'bokor step1',
+			'2' : 'General Talent',
+			'3' : 'bokor step3',
+			'4' : 'General Talent',
+			'5' : 'bokor step5',
+			'6' : 'General Talent',
+			'7' : 'bokor step7',
+			'8' : 'General Talent',
+			'9' : 'bokor step9',
+			'10' : 'bokor step10',
+		}
+	},
+}

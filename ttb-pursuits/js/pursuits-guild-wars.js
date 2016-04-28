afcf1d6d041f5e78d08b1e206ed95a95bb57f608
FreeAlchemist <freealchemist@mail.ru>
2016-04-28 10:39:15 +0300

    Create pursuits-guild-wars.js

---
+++ ttb-pursuits/js/pursuits-guild-wars.js
@@ -0,0 +1,25 @@
+var pursuit = {
+	type:'Guild Wars',
+	faction:'guild',
+	'guild-wars' : {
+		name: 'guild-wars',
+		text:'guild-wars text',
+		gear:'',
+		talent:{
+			name:'educated',
+			text:'Whenever this character...guild-wars'
+		},
+		step:{
+			'1' : 'guild-wars step1',
+			'2' : 'General Talent',
+			'3' : 'guild-wars step3',
+			'4' : 'General Talent',
+			'5' : 'guild-wars step5',
+			'6' : 'General Talent',
+			'7' : 'guild-wars step7',
+			'8' : 'General Talent',
+			'9' : 'guild-wars step9',
+			'10' : 'guild-wars step10',
+		}
+	},
+}

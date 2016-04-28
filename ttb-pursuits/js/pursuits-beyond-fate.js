46d193be3a8461d5948c3e1d25fc6cd6583356e1
FreeAlchemist <freealchemist@mail.ru>
2016-04-28 10:38:46 +0300

    Create pursuits-beyond-fate.js

---
+++ ttb-pursuits/js/pursuits-beyond-fate.js
@@ -0,0 +1,25 @@
+var pursuit = {
+	type:'Beyond Fate',
+	faction:'avatar',
+	'beyond-fate' : {
+		name: 'beyond-fate',
+		text:'beyond-fate text',
+		gear:'',
+		talent:{
+			name:'educated',
+			text:'Whenever this character...beyond-fate'
+		},
+		step:{
+			'1' : 'beyond-fate step1',
+			'2' : 'General Talent',
+			'3' : 'beyond-fate step3',
+			'4' : 'General Talent',
+			'5' : 'beyond-fate step5',
+			'6' : 'General Talent',
+			'7' : 'beyond-fate step7',
+			'8' : 'General Talent',
+			'9' : 'beyond-fate step9',
+			'10' : 'beyond-fate step10',
+		}
+	},
+}

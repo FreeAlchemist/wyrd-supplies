36b554ff633d632fa35e0c4e2bd6b2dc77e27f1f
FreeAlchemist <freealchemist@mail.ru>
2016-04-28 14:42:02 +0300

    Create strategy-basic.js

---
+++ malifaux-strategy/js/strategy-basic.js
@@ -0,0 +1,38 @@
+var strategy = {
+	type:'Basic',
+	'guild' : {
+		name: 'Turf War',
+		text:'Turf War text',
+		setup:'Turf War setup',
+		// special:'',
+		vp:'Turf War VP',
+	},
+	'crows' : {
+		name: 'Reckoning',
+		text:'Reckoning text',
+		// setup:'',
+		// special:'',
+		vp:'Reckoning VP',
+	},
+	'masks' : {
+		name: 'Reconnoiter',
+		text:'Reconnoiter text',
+		setup:'Reconnoiter setup',
+		// special:'',
+		vp:'Reconnoiter VP',
+	},
+	'tomes' : {
+		name: 'Squatter\'s Rights',
+		text:'Squatter\'s Rights text',
+		setup:'Squatter\'s Rights setup',
+		special:'Squatter\'s Rights special',
+		vp:'Squatter\'s Rights VP',
+	},
+	'jockers' : {
+		name: 'Stake A Claim',
+		text:'Stake A Claim text',
+		// setup:'',
+		special:'Stake A Claim special',
+		vp:'Stake A Claim VP',
+	},
+}

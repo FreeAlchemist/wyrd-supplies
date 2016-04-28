ac556963aea70ca79b24204e641e6441a7c63ff9
FreeAlchemist <freealchemist@mail.ru>
2016-04-28 17:14:38 +0300

    Update strategy-cardsCreate.js

--- malifaux-strategy/js/strategy-cardsCreate.js
+++ malifaux-strategy/js/strategy-cardsCreate.js
@@ -1,10 +1,11 @@
 $('body').append($('<div />',{id:'menu'}))
 $('#menu').append($('<a />',{href:'../index.html',text:'Wyrd Supplies'}))
 $('#menu').append($('<a />',{href:'strategy-basic.html',text:'Strategy: Basic'}))
+$('#menu').append($('<a />',{href:'scheme-basic.html',text:'Scheme: Basic'}))
 
 var arr = [];
 
-for (var p in pursuit) {
+for (var p in cards) {
 	arr.push(p);
 }
 
@@ -15,75 +16,63 @@ console.log(arr.length)
 
 var quantity = arr.length;
 
-for (var i = 2; i < quantity; i++) {
-	console.log('PURSUIT #'+i+': ')
-	console.log(pursuit[arr[i]])
-	// console.log(pursuit[i].text)
-	// console.log(pursuit[i].gear)
-	// console.log(pursuit[i].talent['name'])
-	// console.log(pursuit[i].talent['text'])
-
+for (var i = 0; i < quantity; i++) {
 	var cardid = arr[i];
-	var cardfrontid = cardid+'-front';
-	var cardbackid = cardid+'-back';
+	console.log('cards #'+i+': ')
 	console.log(cardid)
-	$('#page').append($('<div />',{class:'card card-front',id:cardfrontid}))
-	$('#'+cardfrontid).append($('<div />',{class:'front1',id:cardfrontid+'-bg1'}))
-	$('#'+cardfrontid).append($('<div />',{class:'front2',id:cardfrontid+'-bg2'}))
-	
-	$('#'+cardfrontid+'-bg1').append($('<div />',{class:'pursuit-title',text:pursuit[cardid].name}))
-	$('#'+cardfrontid+'-bg1').append($('<div />',{class:'pursuit-text',text:pursuit[cardid].text}))
+	console.log(cardid+' is not a number:'+isNaN(cardid))
+	console.log(cards[cardid].name)
+	console.log(cards[cardid].text)
+	console.log(cards[cardid].setup)
+	console.log(cards[cardid].special)
+	console.log(cards[cardid].vp)
 
-	if(pursuit[cardid].gear){
-		$('#'+cardfrontid+'-bg1').append($('<div />',{class:'gear-name',text:'GEAR:'}))
-		$('#'+cardfrontid+'-bg1').append($('<div />',{class:'gear-text',text:pursuit[cardid].gear}))
-	}
 
-	if(pursuit[cardid].talent['name']){
-		$('#'+cardfrontid+'-bg1').append($('<div />',{class:'talent-name',text:pursuit[cardid].talent['name']}))
-		$('#'+cardfrontid+'-bg1').append($('<div />',{class:'talent-text',text:pursuit[cardid].talent['text']}))
-	}
+	$('#page').append($('<div />',{class:'card',id:cardid+'-card'}))
+		$('#'+cardid+'-card').append($('<div />',{class:'front',id:cardid+'-front'}))
 
-	var stepid = cardfrontid+'-bg2'+'-step'
-	// $('#'+cardfrontid+'-bg2').append($('<table />',{class:'text step',id:stepid,cellspacing:'0',cellpadding:'0'}))
-	// $('#'+stepid).append($('<tr />',{id:stepid+'-row-0'}))
-	// $('#'+stepid+'-row-0').append($('<td />',{class:'step-title',text:'#'}))
-	// $('#'+stepid+'-row-0').append($('<td />',{class:'step-title',text:'Talent'}))
+			$('#'+cardid+'-front').append($('<div />',{class:'header',id:cardid+'-header'}))
+				if(isNaN(cardid) == true && cardid != 'always' && cardid != 'doubles'){
+					$('#'+cardid+'-header').append($('<div />',{class:'seal '+cardid,id:cardid+'-seal'}))
+				}
+				else if(cardid == 'always'){
+					$('#'+cardid+'-header').append($('<div />',{class:'seal',id:cardid+'-seal',text:'A'}))
+				}
+				else if(cardid == 'doubles'){
+					$('#'+cardid+'-header').append($('<div />',{class:'seal',id:cardid+'-seal',text:'D'}))
+				}
+				else{
+					$('#'+cardid+'-header').append($('<div />',{class:'seal',id:cardid+'-seal',text:cardid}))
+				}
+			$('#'+cardid+'-header').append($('<div />',{class:'title',id:cardid+'-title',text:cards[cardid].name}))
 
-	// $('#'+cardfrontid+'-bg2').append($('<div />',{class:'gear-text',text:'Step:'}))
-	// $('#'+cardfrontid+'-bg2').append($('<div />',{class:'gear-text',text:'Talent'}))
+			$('#'+cardid+'-front').append($('<div />',{class:'block',id:cardid+'-block'}))
+				$('#'+cardid+'-block').append($('<div />',{class:'flavor',id:cardid+'-flavor',text:cards[cardid].text}))
 
-	$('#'+cardfrontid+'-bg2').append($('<ol />',{class:'step-ol',id:stepid}))
-	$('#'+stepid).append($('<b />',{text:'Talent advancement'}))
+			$('#'+cardid+'-front').append($('<div />',{class:'text',id:cardid+'-text'}))
+				$('#'+cardid+'-text').append($('<p />',{text:'Setup: '+cards[cardid].setup}))
+				$('#'+cardid+'-text').append($('<p />',{text:'Victory points'+cards[cardid].vp}))
 
-	var steparr = [];
-	for (var c in pursuit[cardid].step) {
-		steparr.push(c);
-	}
-	var stepquantity = steparr.length;
-	// console.log(stepquantity)
-	for (var s = 1; s <= stepquantity; s++) {
-		// console.log('STEP #'+s+': ')
-		// console.log(pursuit[arr[i]].step[s])
-		// var steprowid = stepid+'-row-'+s
-		// $('#'+stepid).append($('<tr />',{id:steprowid}))
-		// $('#'+steprowid).append($('<td />',{class:'td-num',text:s}))
-		// $('#'+steprowid).append($('<td />',{text:pursuit[cardid].step[s]}))
+			$('#'+cardid+'-front').append($('<div />',{class:'bottom',id:cardid+'-bottom'}))
+				$('#'+cardid+'-bottom').append($('<div />',{class:'label',text:cards[cardid].type}))
 
-		// $('#'+stepid).append($('<ol />',{id:steprowid}))
-		// $('#'+steprowid).append($('<td />',{class:'td-num',text:s}))
-		$('#'+stepid).append($('<li />',{text:pursuit[cardid].step[s]}))
-
-	};
 };
 
-$('#page').append($('<div />',{class:'row'}))
+// $('#page').append($('<div />',{class:'row'}))
 
-for (var i = quantity-1; i >= 2; i--) {
-	var cardid = arr[i];
-	var cardbackid = cardid+'-back';
-	$('#page').append($('<div />',{class:'card card-back',id:cardbackid}))
-	$('#'+cardbackid).append($('<div />',{class:'back '+cardid,id:cardbackid+'-bg'}))
-	$('#'+cardbackid+'-bg').append($('<div />',{class:'title-back',text:pursuit[cardid].name}))
-	$('#'+cardbackid+'-bg').append($('<div />',{class:'title-back type '+pursuit.faction,text:'\"'+pursuit.type+'\"'}))
-}
+// <div class="card">
+// 	<div class="back">
+// 		<div class="top_label">scheme</div>
+// 		<div class="logo"></div>
+// 		<div class="bottom_label">scheme</div>
+// 		</div>
+// </div>
+
+// for (var i = quantity-1; i >= 0; i--) {
+// 	var cardid = arr[i];
+// 	$('#page').append($('<div />',{class:'card',id:cardid+'-card'}))
+// 		$('#'+cardid+'-card').append($('<div />',{class:'back',id:cardid+'-back'}))
+// 			$('#'+cardid+'-back').append($('<div />',{class:'top_label',text:cards[cardid].type}))
+// 			$('#'+cardid+'-back').append($('<div />',{class:'logo'}))
+// 			$('#'+cardid+'-back').append($('<div />',{class:'bottom_label',text:cards[cardid].type}))
+// }

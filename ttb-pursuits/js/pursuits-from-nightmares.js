99d603a7b6cb6062d9867f5850c5dcc7c428fc96
FreeAlchemist <freealchemist@mail.ru>
2016-04-28 10:39:01 +0300

    Create pursuits-from-nightmares.js

---
+++ ttb-pursuits/js/pursuits-from-nightmares.js
@@ -0,0 +1,25 @@
+var pursuit = {
+	type:'From Nightmares',
+	faction:'neverborn',
+	'from-nightmares' : {
+		name: 'from-nightmares',
+		text:'from-nightmares text',
+		gear:'',
+		talent:{
+			name:'educated',
+			text:'Whenever this character...from-nightmares'
+		},
+		step:{
+			'1' : 'from-nightmares step1',
+			'2' : 'General Talent',
+			'3' : 'from-nightmares step3',
+			'4' : 'General Talent',
+			'5' : 'from-nightmares step5',
+			'6' : 'General Talent',
+			'7' : 'from-nightmares step7',
+			'8' : 'General Talent',
+			'9' : 'from-nightmares step9',
+			'10' : 'from-nightmares step10',
+		}
+	},
+}

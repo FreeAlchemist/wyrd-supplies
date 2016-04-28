429fa6e4acbaa234bf1333215b374a82bb6a73b3
FreeAlchemist <freealchemist@mail.ru>
2016-04-28 10:36:56 +0300

    Create pursuits-advanced.js

---
+++ ttb-pursuits/js/pursuits-advanced.js
@@ -0,0 +1,20 @@
+var pursuit = {
+	type:'Advanced',
+	faction:'advanced',
+	'death-marshal' : {
+		name: 'death-marshal',
+		text:'death-marshal text',
+		gear:'',
+		talent:{
+			name:'educated',
+			text:'Whenever this character...death-marshal'
+		},
+		step:{
+			'1' : 'Peacebringer',
+			'2' : 'Coffin',
+			'3' : 'Special Dispensation',
+			'4' : 'Pine Box',
+			'5' : 'Flaming Head',
+		}
+	},
+}

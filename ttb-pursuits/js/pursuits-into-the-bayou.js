f3bba499205ec7eafdea29d580b6bd2477e13e47
FreeAlchemist <freealchemist@mail.ru>
2016-08-30 17:24:06 +0300

    Add files via upload

--- ttb-pursuits/js/pursuits-into-the-bayou.js
+++ ttb-pursuits/js/pursuits-into-the-bayou.js
@@ -18,4 +18,123 @@ var pursuit = {
 			'10' : 'bokor step10',
 		}
 	},
+	'boomer' : {
+		name: 'boomer',
+		text:'boomer text',
+		gear:'',
+		step:{
+			'1' : 'boomer step1',
+			'2' : 'Общий талант',
+			'3' : 'boomer step3',
+			'4' : 'Общий талант',
+			'5' : 'boomer step5',
+			'6' : 'Общий талант',
+			'7' : 'boomer step7',
+			'8' : 'Общий талант',
+			'9' : 'boomer step9',
+			'10' : 'boomer step10',
+		}
+	},
+	'boozer' : {
+		name: 'boozer',
+		text:'boozer text',
+		gear:'',
+		step:{
+			'1' : 'boozer step1',
+			'2' : 'Общий талант',
+			'3' : 'boozer step3',
+			'4' : 'Общий талант',
+			'5' : 'boozer step5',
+			'6' : 'Общий талант',
+			'7' : 'boozer step7',
+			'8' : 'Общий талант',
+			'9' : 'boozer step9',
+			'10' : 'boozer step10',
+		}
+	},
+	'buckaroo' : {
+		name: 'buckaroo',
+		text:'buckaroo text',
+		gear:'',
+		step:{
+			'1' : 'buckaroo step1',
+			'2' : 'Общий талант',
+			'3' : 'buckaroo step3',
+			'4' : 'Общий талант',
+			'5' : 'buckaroo step5',
+			'6' : 'Общий талант',
+			'7' : 'buckaroo step7',
+			'8' : 'Общий талант',
+			'9' : 'buckaroo step9',
+			'10' : 'buckaroo step10',
+		}
+	},
+	'copycat' : {
+		name: 'copycat',
+		text:'copycat text',
+		gear:'',
+		step:{
+			'1' : 'copycat step1',
+			'2' : 'Общий талант',
+			'3' : 'copycat step3',
+			'4' : 'Общий талант',
+			'5' : 'copycat step5',
+			'6' : 'Общий талант',
+			'7' : 'copycat step7',
+			'8' : 'Общий талант',
+			'9' : 'copycat step9',
+			'10' : 'copycat step10',
+		}
+	},
+	'pugilist' : {
+		name: 'pugilist',
+		text:'pugilist text',
+		gear:'',
+		step:{
+			'1' : 'pugilist step1',
+			'2' : 'Общий талант',
+			'3' : 'pugilist step3',
+			'4' : 'Общий талант',
+			'5' : 'pugilist step5',
+			'6' : 'Общий талант',
+			'7' : 'pugilist step7',
+			'8' : 'Общий талант',
+			'9' : 'pugilist step9',
+			'10' : 'pugilist step10',
+		}
+	},
+	'swineherd' : {
+		name: 'swineherd',
+		text:'swineherd text',
+		gear:'',
+		step:{
+			'1' : 'swineherd step1',
+			'2' : 'Общий талант',
+			'3' : 'swineherd step3',
+			'4' : 'Общий талант',
+			'5' : 'swineherd step5',
+			'6' : 'Общий талант',
+			'7' : 'swineherd step7',
+			'8' : 'Общий талант',
+			'9' : 'swineherd step9',
+			'10' : 'swineherd step10',
+		}
+	},
+	'trapper' : {
+		name: 'trapper',
+		text:'trapper text',
+		gear:'',
+		step:{
+			'1' : 'trapper step1',
+			'2' : 'Общий талант',
+			'3' : 'trapper step3',
+			'4' : 'Общий талант',
+			'5' : 'trapper step5',
+			'6' : 'Общий талант',
+			'7' : 'trapper step7',
+			'8' : 'Общий талант',
+			'9' : 'trapper step9',
+			'10' : 'trapper step10',
+		}
+	},
 }

6bfaf62a36da51d47d59e63c971fadfa85505f6d
FreeAlchemist <freealchemist@mail.ru>
2017-11-21 18:00:01 +0300

    Add files via upload

--- ttb-pursuits/js/pursuits-above-the-law.js
+++ ttb-pursuits/js/pursuits-above-the-law.js
@@ -2,6 +2,8 @@ var set = 'Above the Law';
 var faction = 'guild';
 var pursuit = [
 	'commander',
-	// 'above-the-law1',
-	// 'above-the-law2',
+	'marksman',
+	'bureaucrat',
+	'magewright',
+	'gunner',
 ]
